1c1c5590f358a2f430aad12caba762c644f3fa01
Wisse <wissehes@gmail.com>
2021-07-11 17:35:45 +0200

    Create some styles

---
+++ structs/LightButtonStyles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  constainer: {
+    flex: 1,
+    height: 100,
+    justifyContent: "center",
+    width: "100%",
+    // backgroundColor: "gray",
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 25,
+    padding: 10,
+  },
+  ip: {
+    fontSize: 15,
+    padding: 10,
+  },
+  icon: {
+    position: "absolute",
+    right: 0,
+    padding: 10,
+  },
+});
